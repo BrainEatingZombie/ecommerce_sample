fa7a3c46aaf273af2740c6d3da9c6e67c5c25668
BrainEatingZombie <salemfahad1@gmail.com>
2019-08-21 19:10:17 +0600

    Added admin login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Auth;
+use Session;
 class AdminController extends Controller
 {
-    public function login(){
+    public function login(Request $request){
+    	if ($request->isMethod('post')) {
+    		$data = $request->input();
+    		if (Auth::attempt(['email'=>$data['email'],'password'=>$data['password'],'admin'=>'1'])) {
+    			echo "Success"; die;
+    		}
+    		else{
+    			echo "Failed"; die;
+    		}
+    	}
     	return view('admin.admin_login');
     }
 }
