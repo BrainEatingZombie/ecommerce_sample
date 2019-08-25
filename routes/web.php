395da099f8fe8fb8bf26e788621b48a7daeb2b5b
BrainEatingZombie <salemfahad1@gmail.com>
2019-08-26 01:59:58 +0600

    Add Category page added

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,9 @@ Route::group(['middleware' => ['auth']], function(){
 	Route::get('/admin/settings','AdminController@settings');
 	Route::get('/admin/check-pwd','AdminController@chkPassword');
 	Route::match(['get','post'],'/admin/update-pwd','AdminController@updatePassword');
+
+	// Categories Routes(Admin)
+	Route::match(['get','post'],'/admin/add-category','CategoryController@addCategory');
 		
 });
 
