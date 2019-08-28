e8c2f54e8a0b3505958e9bd00e77da16ff516697
BrainEatingZombie <salemfahad1@gmail.com>
2019-08-29 00:02:35 +0600

    Added a View categories page

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -19,4 +19,8 @@ class CategoryController extends Controller
     	}
     	return view('admin.categories.add_category');
     }
+
+    public function viewCategories(){
+        return view('admin.categories.view_categories');
+    }
 }
