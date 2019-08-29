488b317436c38b76f552ecf89e6ce18c808488b7
BrainEatingZombie <salemfahad1@gmail.com>
2019-08-29 12:04:04 +0600

    Added paging and search functionality in view products page

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -16,11 +16,14 @@ class CategoryController extends Controller
     		$category->description = $data['description'];
     		$category->url = $data['url'];
     		$category->save();
+            return redirect('/admin/view-categories')->with('flash_message_success', 'Category added Successfully!');
     	}
     	return view('admin.categories.add_category');
     }
 
     public function viewCategories(){
-        return view('admin.categories.view_categories');
+        $categories = Category::get();
+        $categories = json_decode(json_encode($categories));
+        return view('admin.categories.view_categories')->with(compact('categories'));
     }
 }
